3b72a767e897e4a0cc4671419bd255011cbe1cb7
SEJUIN <80575726+SEJUIN@users.noreply.github.com>
2021-04-15 23:46:18 +0900

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool fDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        fDown = Input.GetButton("SpeedDoping");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed *(fDown ? 1f : 0.5f )* Time.deltaTime;
+
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+        anim.SetBool("isSpeedDoping",fDown);
+
+        transform.LookAt(transform.position + moveVec);
+    }
+}
