aef3b93865bf351a8b8c1fdb78206d5e3daec5d3
SEJUIN <80575726+SEJUIN@users.noreply.github.com>
2021-04-17 04:13:56 +0900

    Map Handrail and Base operation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float rotationSpeed;
     float hAxis;
     float vAxis;
     bool fDown;
+    Rigidbody body;
 
     Vector3 moveVec;
+    float curChaAngle;
 
     Animator anim;
 
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        body = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -24,13 +28,37 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         fDown = Input.GetButton("SpeedDoping");
 
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        moveVec = new Vector3(hAxis, 0, vAxis);
 
-        transform.position += moveVec * speed *(fDown ? 1f : 0.5f )* Time.deltaTime;
+        if (moveVec != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveVec.x, moveVec.z) * Mathf.Rad2Deg;
+
+            if (90.0f > angle && angle > -90.0f)
+            {
+                angle = angle * rotationSpeed * Time.deltaTime;
+                transform.Rotate(Vector3.up, angle);
+            }
+        }
+
+        transform.Translate(moveVec * speed * (fDown ? 1f : 0.5f) * Time.deltaTime);
+
+        //moveVec = transform.forward * moveVec.z + transform.right * moveVec.x;
+
+        //body.velocity = moveVec * speed * Time.fixedDeltaTime;
+
+        //transform.position += moveVec * speed *(fDown ? 1f : 0.5f )* Time.deltaTime;
 
         anim.SetBool("isWalk", moveVec != Vector3.zero);
         anim.SetBool("isSpeedDoping",fDown);
 
-        transform.LookAt(transform.position + moveVec);
+        //if(vAxis >= 0)
+        //    transform.LookAt(transform.position + moveVec);
+
+        //if(hAxis == 0 || vAxis == 0)
+        //{
+        //    curChaAngle = transform.rotation.y;
+        //    //transform.localRotation *= Quaternion.Euler(0, curChaAngle, 0);
+        //}
     }
 }
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    //    public Vector3 offset;
+    public float dist = 10.0f;
+    public float height = 5.0f;
+    public float smoothRotate = 5.0f;
+
+    void LateUpdate()
+    {
+        float curryAngle = Mathf.LerpAngle(transform.eulerAngles.y, target.eulerAngles.y, smoothRotate * Time.deltaTime);
+        
+
+        Quaternion rot = Quaternion.Euler(0, curryAngle, 0);
+
+        transform.position = target.position - (rot * Vector3.forward * dist) + (Vector3.up * height);
+
+        transform.LookAt(target);
+    }
+
+    void Update()
+    {
+        //transform.position = target.position + offset;
+    }
+}
